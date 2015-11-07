7a629686eed71758f52ed945181202caf51907c8
Jonathan Dye <jdye@us.ibm.com>
2015-11-07 15:19:21 -0700

    ethbuilds postscript

---
+++ eth-saltmaster-deb.sh
@@ -0,0 +1,118 @@
+#!/bin/bash -eux
+
+# Update the box
+apt-get -y update
+apt-get -y install linux-headers-$(uname -r) build-essential sudo
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
+apt-get -y install curl unzip
+
+# Set up sudo
+grep -q 'secure_path' /etc/sudoers || sed -i -e '/Defaults\s\+env_reset/a Defaults\tsecure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"' /etc/sudoers
+sed -i -e 's/^%sudo.*/%sudo ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
+
+# Tweak sshd to prevent DNS resolution (speed up logins)
+echo "UseDNS no" >> /etc/ssh/sshd_config
+echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
+
+# Remove 5s grub timeout to speed up booting
+cat <<EOF > /etc/default/grub
+# If you change this file, run 'update-grub' afterwards to update
+# /boot/grub/grub.cfg.
+
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX="debian-installer=en_US"
+EOF
+
+update-grub
+
+apt-get -y update
+apt-get -y install sudo linux-headers-$(uname -r) build-essential
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
+apt-get -y install curl unzip
+
+# Set up sudo
+grep -q 'secure_path' /etc/sudoers || sed -i -e '/Defaults\s\+env_reset/a Defaults\tsecure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"' /etc/sudoers
+sed -i -e 's/^%sudo.*/%sudo ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
+
+# Tweak sshd to prevent DNS resolution (speed up logins)
+echo "UseDNS no" >> /etc/ssh/sshd_config
+echo "GSSAPIAuthentication no" >> /etc/ssh/sshd_config
+
+# Remove 5s grub timeout to speed up booting
+cat <<EOF > /etc/default/grub
+# If you change this file, run 'update-grub' afterwards to update
+# /boot/grub/grub.cfg.
+
+GRUB_DEFAULT=0
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`
+GRUB_CMDLINE_LINUX_DEFAULT="quiet"
+GRUB_CMDLINE_LINUX="debian-installer=en_US"
+EOF
+
+update-grub
+
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+apt-get install -y apt-transport-https || /bin/true
+
+echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
+
+apt-get update
+apt-cache policy docker-engine | egrep '^        ' | grep -v 'apt.dockerproject.org' && ( echo "docker engine is not being pulled from the right repository"; exit 1 )
+
+apt-get install -y docker-engine=1.8.3-0~jessie
+
+mkdir /opt || /bin/true
+cd /opt 
+curl -L https://github.com/coreos/etcd/releases/download/v2.2.1/etcd-v2.2.1-linux-amd64.tar.gz | tar -xvz
+ln -s etcd* etcd
+cd /opt/etcd/
+ln -s $(pwd)/etcd /usr/bin/
+
+mkdir -p /opt/calico-0.9.0 || /bin/true
+cd /opt
+ln -s calico* calico
+cd calico
+curl -L https://github.com/projectcalico/calico-docker/releases/download/v0.9.0/calicoctl > calicoctl
+chmod +x calicoctl
+ln -s $(pwd)/calicoctl /usr/bin/
+
+cat <<\EOF >> /etc/sysctl.conf
+
+# added by packer
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+EOF
+
+# Clean up
+apt-get -y remove linux-headers-$(uname -r) build-essential
+apt-get -y autoremove
+apt-get -y purge $(dpkg --list |grep '^rc' |awk '{print $2}')
+apt-get -y purge $(dpkg --list |egrep 'linux-image-[0-9]' |awk '{print $3,$2}' |sort -nr |tail -n +2 |grep -v $(uname -r) |awk '{ print $2}')
+apt-get -y clean
+
+# Cleanup Virtualbox
+rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
+
+# Cleanup Chef
+rm -f /tmp/chef*deb
+
+# Removing leftover leases and persistent rules
+echo "cleaning up dhcp leases"
+rm /var/lib/dhcp/*
+
+# Make sure Udev doesn't block our network
+mkdir /etc/udev/rules.d/70-persistent-net.rules
+rm /lib/udev/rules.d/75-persistent-net-generator.rules
+rm -rf /dev/.udev/ /var/lib/dhcp3/*
+echo "pre-up sleep 2" >> /etc/network/interfaces
+
+echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
+echo "pre-up sleep 2" >> /etc/network/interfaces
